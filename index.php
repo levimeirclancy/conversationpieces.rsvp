b74cde57af7b1438d3d0cb43b40d28c1950beb54
לוי מאיר קלנסי <32032952+levimeirclancy@users.noreply.github.com>
2024-12-23 19:30:54 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <?
 
-echo "Hello";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+echo "Conversation Pieces";
+echo "<br>";
+echo "Conversation Pieces fosters meaningful dialogue and reflection on Jewish identity, heritage, and resilience through throught-provoking artifacts.";
+
+echo "<br>";
+echo "<br>";
+
+echo "About me";
+echo "<br>";
+echo "Levi Meir Clancy is a Jewish, Ryukyuan, and LGBTQ educator who spent almost eight years based in the Kurdistan Region of Iraq, where he used object-oriented education techniques to bridge polarizing sectarian divides. Now based in the Bay Area, Levi Meir is an ADL Glass Leadership Instute fellow, an album of Zioness' Pride Fellowship, and the creator of Conversation Pieces.";
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
 
 ?>
